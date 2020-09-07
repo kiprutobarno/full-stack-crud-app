78b8df6b2b26af3123da9175a115ec8af38c3be9
ywalakamar <barxwells@gmail.com>
2020-09-07 21:28:07 +0300

    feature(employees):Add fetch all employees feature

--- src/main/java/com/example/backend/controller/employeeController.java
+++ src/main/java/com/example/backend/controller/employeeController.java
@@ -6,6 +6,7 @@ import com.example.backend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,10 @@ public class employeeController {
     @PostMapping("/employees")
     public ResponseEntity<?> createEmployee(@RequestBody EmployeeDTO employee) {
         return ResponseEntity.ok(service.add(employee));
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<?> getEmployees() {
+        return ResponseEntity.ok(service.getAllEmployees());
     }
 }
